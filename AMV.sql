0f54acb70e9b3b0d5a0aab94ed3367dd0627d2b8
ayberk-ka <ayberk.mh@gmail.com>
2021-10-22 13:14:13 +0200

    laget leie kontrakt table og SELECT som viser oss antal dager til leie periode

--- AMV.sql
+++ AMV.sql
@@ -42,6 +42,31 @@ CREATE TABLE utstyr
 );
 INSERT INTO utstyr (utstyr_type_id, utstyr_navn, leie_kostnad, status, bruk_info)
 VALUES (1,'TRUCK',200.30,true,'du trenger opplæring for å bruke truck ta kontakt med admin');
+
+CREATE TABLE leie_kontrakt
+    (
+   leie_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   ansatt_id INT NOT NULL ,
+   utstyr_id INT NOT NULL ,
+   start_leie_dato DATE NOT NULL ,
+   tilbake_dato DATE NOT NULL,
+   betalt TINYINT (1),
+   total_kostnad DECIMAL(5,2),
+   tilstandsvurdering VARCHAR(60),
+   CONSTRAINT FK_ansatt_leie_kontrakt
+    FOREIGN KEY (ansatt_id) REFERENCES ansatt(ansatt_id),
+
+    CONSTRAINT FK_utstyr_leie_kontrakt
+    FOREIGN KEY (utstyr_id) REFERENCES utstyr(utstyr_id)
+);
+INSERT INTO leie_kontrakt (ansatt_id, utstyr_id, start_leie_dato, tilbake_dato, betalt, total_kostnad, tilstandsvurdering)
+VALUES
+(1,1,'2021,10,15','2021,10,22',false,254.50,'bra tilstand ');
+
 SELECT * FROM ansatt;
 SELECT * FROM utstyr_type;
 SELECT * FROM utstyr;
+SELECT * FROM leie_kontrakt;
+SELECT tilbake_dato - start_leie_dato as total_dager   FROM leie_kontrakt,utstyr ;
+
+
